7f131a71660f39e89c1b67608392920c6753af2e
Manuel Gimenez <manugimz24@gmail.com>
2022-05-10 08:10:21 -0300

    reemplazamos una parte del  texto con replace

---
+++ ejercicio-1/reemplazar_str.js
@@ -0,0 +1,5 @@
+/*En este primer ejercicio tienes que crear una cadena de texto con una frase, por ejemplo: “El perro de mi vecina es muy molesto” y reemplazar la palabra vecina por vecino. Por consola tienes que mostrar la frase con las palabras cambiadas.
+*/
+let str_1 = 'El perro de mi vecina es muy molesto'
+console.log(str_1.replace('vecina', 'vecino'));
+//console.log(str_1.replace(/vecina/g, 'vecino')); en caso de querer reemplazar varias veces la misma palabra!
