38dafabf607e0b5c766876f6f73df27b4368c023
Manuel Gimenez <manugimz24@gmail.com>
2022-06-01 07:10:00 -0300

    modulo 11 POO, creacion de class Estudiante y metodo getDate

---
+++ modulo-11-POO-en-Js/index.js
@@ -0,0 +1,21 @@
+/*Crea un nuevo fichero JS que contenga las siguientes líneas
+- Una clase llamada "Estudiante" que tenga:
+- Una variable llamada nombre
+- Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
+- Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+- Crea una nueva instancia de "Estudiante"
+- Haz la llamada al método obtenDatos */
+class Estudiante {
+    nombre = 'nombre'
+    asignaturas = ['Javascript', 'HTML', 'CSS']
+   
+    obtenDatos (){
+        return {
+            nombre : this.nombre,
+            asignaturas : this.asignaturas,
+        }
+    }
+}
+const nuevoEstudiante = new Estudiante
+nuevoEstudiante.nombre = 'Manu'
+console.log(nuevoEstudiante.obtenDatos())
